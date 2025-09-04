369dbd6c799ad0f0e32100ee49a12ae1cff83767
burhankhanzada <burhankhanzada199888@gmail.com>
2025-09-04 15:25:54 +0200

    servo frame ready working

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,7 @@ use winit::dpi;
 
 use slint::winit_030::WinitWindowAccessor;
 
-use slint::RenderingState;
-
-use raw_window_handle::{HasDisplayHandle, HasWindowHandle};
-
-use servo::{
-    RenderingContext, Servo, ServoBuilder, WebView, WebViewBuilder, WindowRenderingContext,
-};
+use servo::{Servo, ServoBuilder, SoftwareRenderingContext, WebView, WebViewBuilder};
 
 slint::slint! {
 
@@ -25,9 +19,14 @@ slint::slint! {
 
         in property <image> web_content <=> image.source;
 
-        image :=  Image {
-            width: 100%;
-            height: 100%;
+        VerticalLayout {
+            Text {
+                text: "Hello from Slint";
+            }
+            image := Image {
+                width: 100%;
+                height: 100%;
+            }
         }
     }
 }
@@ -37,6 +36,7 @@ struct AppDelegate {
 
 impl servo::WebViewDelegate for AppDelegate {
     fn notify_new_frame_ready(&self, _webview: WebView) {
+        eprintln!("New frame ready");
         self.app.window().request_redraw();
     }
 }
@@ -45,37 +45,30 @@ fn main() {
     let app = MyApp::new().unwrap();
     let app_weak = app.as_weak();
 
-    //let window = app.window();
+    let (waker_sender, waker_receiver) = smol::channel::unbounded::<()>();
 
     let servo: Rc<RefCell<Option<Servo>>> = Rc::new(RefCell::new(None));
-    let servo_clone = servo.clone();
 
     let webview: Rc<RefCell<Option<WebView>>> = Rc::new(RefCell::new(None));
     let webview_clone = webview.clone();
 
     slint::spawn_local({
         let app_weak = app_weak.clone();
+        let servo_clone = servo.clone();
+
         async move {
             let app = app_weak.upgrade().unwrap();
             let winit_window = app.window().winit_window().await.unwrap();
-            let slint_window_handle = app.window().window_handle();
 
-            let window_handle = slint_window_handle.window_handle().unwrap();
-            let display_handle = slint_window_handle.display_handle().unwrap();
-
-            let window_size = app.window().size();
+            let window_size = winit_window.inner_size();
             let size = dpi::PhysicalSize::new(window_size.width, window_size.height);
 
-            let rendering_context =
-                WindowRenderingContext::new(display_handle, window_handle, size).unwrap();
+            let rendering_context = SoftwareRenderingContext::new(size).unwrap();
             let rendering_context_rc = Rc::new(rendering_context);
 
-            let offscreen_context = rendering_context_rc.offscreen_context(size);
-            let offscreen_context_rc = Rc::new(offscreen_context);
-
-            let _ = offscreen_context_rc.make_current();
-
-            let servo_instance = ServoBuilder::new(rendering_context_rc).build();
+            let servo_instance = ServoBuilder::new(rendering_context_rc)
+                .event_loop_waker(Box::new(Waker::new(waker_sender)))
+                .build();
             servo_instance.setup_logging();
 
             let delegate = Rc::new(AppDelegate { app: app });
@@ -92,54 +85,38 @@ fn main() {
     })
     .unwrap();
 
-    /* 
-    window
-        .set_rendering_notifier(move |state, _graphics_api| match state {
-            RenderingState::RenderingSetup => {
-                let app = app_weak.upgrade().unwrap();
+    slint::spawn_local({
+        let servo_clone = servo.clone();
 
-                let window = app.window();
-                let slint_window_handle = window.window_handle();
-
-                let window_handle = slint_window_handle.window_handle().unwrap();
-                let display_handle = slint_window_handle.display_handle().unwrap();
-
-                let window_size = window.size();
-                let size = dpi::PhysicalSize::new(window_size.width, window_size.height);
-
-                let rendering_context =
-                    WindowRenderingContext::new(display_handle, window_handle, size).unwrap();
-                let rendering_context_rc = Rc::new(rendering_context);
-
-                let offscreen_context = rendering_context_rc.offscreen_context(size);
-                let offscreen_context_rc = Rc::new(offscreen_context);
-
-                let _ = offscreen_context_rc.make_current();
-
-                let servo_instance = ServoBuilder::new(offscreen_context_rc).build();
-                servo_instance.setup_logging();
-
-                let delegate = Rc::new(AppDelegate { app: app });
-
-                let url = Url::parse("https://example.com/").unwrap();
-                let webview_instance = WebViewBuilder::new(&servo_instance)
-                    .url(url)
-                    .delegate(delegate.clone())
-                    .build();
-
-                *webview_clone.borrow_mut() = Some(webview_instance);
-                *servo_clone.borrow_mut() = Some(servo_instance);
-            }
-            RenderingState::RenderingTeardown => {
-                *webview_clone.borrow_mut() = None;
-                if let Some(servo_instance) = servo_clone.borrow_mut().take() {
-                    servo_instance.deinit();
+        async move {
+            loop {
+                let _ = waker_receiver.recv().await;
+                if let Some(servo) = servo_clone.borrow().as_ref() {
+                    servo.spin_event_loop();
                 }
             }
-            _ => {}
-        })
-        .unwrap();
-*/
+        }
+    })
+    .unwrap();
 
     app.run().unwrap();
+}
+
+#[derive(Clone)]
+struct Waker(smol::channel::Sender<()>);
+
+impl Waker {
+    fn new(sender: smol::channel::Sender<()>) -> Self {
+        Self(sender)
+    }
+}
+
+impl embedder_traits::EventLoopWaker for Waker {
+    fn clone_box(&self) -> Box<dyn embedder_traits::EventLoopWaker> {
+        Box::new(Self(self.0.clone()))
+    }
+
+    fn wake(&self) {
+        self.0.send_blocking(()).unwrap();
+    }
 }
