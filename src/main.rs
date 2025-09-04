525e4861ad196ee117dc40886a98bba7b0f7d58d
burhankhanzada <burhankhanzada199888@gmail.com>
2025-09-04 23:16:17 +0200

    white background

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,18 @@
 use std::cell::RefCell;
 use std::rc::Rc;
 
+use euclid::Point2D;
+use image::{DynamicImage, ImageFormat};
 use url::Url;
 
+use webrender_api::units::DeviceIntRect;
 use winit::dpi;
 
 use slint::winit_030::WinitWindowAccessor;
 
-use servo::{Servo, ServoBuilder, SoftwareRenderingContext, WebView, WebViewBuilder};
+use servo::{
+    RenderingContext, Servo, ServoBuilder, SoftwareRenderingContext, WebView, WebViewBuilder,
+};
 
 slint::slint! {
 
@@ -31,13 +36,14 @@ slint::slint! {
     }
 }
 struct AppDelegate {
-    app: MyApp,
+    rendering_context: Rc<SoftwareRenderingContext>,
 }
 
 impl servo::WebViewDelegate for AppDelegate {
     fn notify_new_frame_ready(&self, _webview: WebView) {
-        eprintln!("New frame ready");
-        self.app.window().request_redraw();
+        eprintln!("New frame ready {:?}", _webview.page_title());
+        eprintln!("{}", _webview.paint());
+        output_image(&self.rendering_context);
     }
 }
 
@@ -66,12 +72,14 @@ fn main() {
             let rendering_context = SoftwareRenderingContext::new(size).unwrap();
             let rendering_context_rc = Rc::new(rendering_context);
 
-            let servo_instance = ServoBuilder::new(rendering_context_rc)
+            let servo_instance = ServoBuilder::new(rendering_context_rc.clone())
                 .event_loop_waker(Box::new(Waker::new(waker_sender)))
                 .build();
             servo_instance.setup_logging();
 
-            let delegate = Rc::new(AppDelegate { app: app });
+            let delegate = Rc::new(AppDelegate {
+                rendering_context: rendering_context_rc,
+            });
 
             let url = Url::parse("https://example.com/").unwrap();
             let webview_instance = WebViewBuilder::new(&servo_instance)
@@ -119,4 +127,25 @@ impl embedder_traits::EventLoopWaker for Waker {
     fn wake(&self) {
         self.0.send_blocking(()).unwrap();
     }
+}
+
+pub fn output_image(rendering_context: &Rc<SoftwareRenderingContext>) {
+    rendering_context.present();
+
+    let size = rendering_context.size2d().to_i32();
+    let rect = DeviceIntRect::from_origin_and_size(Point2D::origin(), size);
+
+    let image = rendering_context.read_to_image(rect).unwrap();
+
+    let image_size = image.dimensions();
+
+    println!("{:?} from {:#?}", image_size, rect);
+
+    let output_path = "./output.png";
+
+    let image_format = ImageFormat::from_path(output_path).unwrap_or(ImageFormat::Png);
+
+    DynamicImage::ImageRgba8(image)
+        .save_with_format(output_path, image_format)
+        .unwrap();
 }
