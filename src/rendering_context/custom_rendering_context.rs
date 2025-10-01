cbe5ddd6e7754f4ef55e6c5832806566c5819578
Ashley Ruglys <ashley.ruglys@gmail.com>
2025-10-01 11:37:05 +0200

    Implement using gl_generator

--- src/rendering_context/custom_rendering_context.rs
+++ src/rendering_context/custom_rendering_context.rs
@@ -2,6 +2,7 @@ use std::{cell::Cell, rc::Rc, sync::Arc};
 
 use euclid::default::Size2D;
 
+use crate::gl_bindings as gl;
 use image::RgbaImage;
 use servo::RenderingContext;
 use slint::wgpu_26::wgpu;
@@ -13,9 +14,7 @@ use surfman::{
     chains::{PreserveBuffer, SwapChain},
 };
 
-use crate::rendering_context::{
-    surfman_context::SurfmanRenderingContext,
-};
+use crate::rendering_context::surfman_context::SurfmanRenderingContext;
 
 pub struct CustomRenderingContext {
     pub size: Cell<PhysicalSize<u32>>,
@@ -61,10 +60,114 @@ impl CustomRenderingContext {
         &self,
         wgpu_device: &wgpu::Device,
         wgpu_queue: &wgpu::Queue,
-    ) -> Result<wgpu::Texture, Error> { 
-        panic!()
+    ) -> Result<wgpu::Texture, Error> {
+        let device = &self.surfman_rendering_info.device.borrow();
+        let mut context = self.surfman_rendering_info.context.borrow_mut();
+
+        let surface = device.unbind_surface_from_context(&mut context)?.unwrap();
+
+        let tex_size = self.size.get();
+        let height = tex_size.height as i32;
+
+        let width = tex_size.width as i32;
+
+        let texture = unsafe {
+            use glow::HasContext;
+
+            let vulkan_device = wgpu_device.as_hal::<wgpu::wgc::api::Vulkan>().unwrap();
+
+            let (vulkan_texture, memory_handle, size) = vulkan_device
+                .texture_funky(&wgpu_hal::TextureDescriptor {
+                    label: None,
+                    size: wgpu::Extent3d {
+                        width: tex_size.width,
+                        height: tex_size.height,
+                        depth_or_array_layers: 1,
+                    },
+                    format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                    dimension: wgpu::TextureDimension::D2,
+                    mip_level_count: 1,
+                    sample_count: 1,
+                    usage: wgpu::TextureUses::RESOURCE | wgpu::TextureUses::COLOR_TARGET,
+                    view_formats: vec![],
+                    memory_flags: wgpu_hal::MemoryFlags::empty(),
+                })
+                .unwrap();
+
+            let gl = &self.surfman_rendering_info.glow_gl;
+
+            let gl_with_extensions =
+                gl::Gl::load_with(|function_name| device.get_proc_address(&context, function_name));
+
+            let mut memory_object = 0;
+            gl_with_extensions.CreateMemoryObjectsEXT(1, &mut memory_object);
+            // We're using a dedicated allocation.
+            gl_with_extensions.MemoryObjectParameterivEXT(memory_object, gl::DEDICATED_MEMORY_OBJECT_EXT, &1);
+            gl_with_extensions.ImportMemoryFdEXT(
+                memory_object,
+                size,
+                gl::HANDLE_TYPE_OPAQUE_FD_EXT,
+                memory_handle,
+            );
+            // Create a texture and bind it to the imported memory.
+            let mut texture = gl.create_texture().unwrap();
+            gl.bind_texture(gl::TEXTURE_2D, Some(texture));
+            gl_with_extensions.TexStorageMem2DEXT(
+                gl::TEXTURE_2D,
+                1,
+                gl::RGBA8,
+                width,
+                height,
+                memory_object,
+                0,
+            );
+
+
+            let mut framebuffer = gl.create_framebuffer().unwrap();
+            // Bind a destination framebuffer targeting our texture.
+            gl.bind_framebuffer(gl::FRAMEBUFFER, Some(framebuffer));
+            gl.framebuffer_texture_2d(
+                gl::FRAMEBUFFER,
+                gl::COLOR_ATTACHMENT0,
+                gl::TEXTURE_2D,
+                Some(texture),
+                0,
+            );
+            gl.clear_color(0.7, 0.6, 0.5, 1.0);
+            gl.clear(gl::COLOR_BUFFER_BIT);
+            gl.flush();
+
+
+            wgpu_device.create_texture_from_hal::<wgpu::wgc::api::Vulkan>(
+                vulkan_texture,
+                &wgpu::TextureDescriptor {
+                    label: None,
+                    size: wgpu::Extent3d {
+                        width: tex_size.width,
+                        height: tex_size.height,
+                        depth_or_array_layers: 1,
+                    },
+                    format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                    dimension: wgpu::TextureDimension::D2,
+                    mip_level_count: 1,
+                    sample_count: 1,
+                    usage: wgpu::TextureUsages::TEXTURE_BINDING
+                        | wgpu::TextureUsages::RENDER_ATTACHMENT,
+                    view_formats: &[],
+                },
+            )
+        };
+
+        let _ = device
+            .bind_surface_to_context(&mut context, surface)
+            .map_err(|(err, mut surface)| {
+                let _ = device.destroy_surface(&mut context, &mut surface);
+                err
+            });
+
+        Ok(texture)
     }
-    
+
     /*
     pub fn get_wgpu_texture_from_metal(
         &self,
